1987cf2454854d94a4c13f8c204aede45c98af1a
Camel Aissani <camel.aissani@nuageprive.fr>
2016-09-09 22:56:20 +0200

    shrank frontexpress.min.js - added frontexpress.js - added those files in .npmignore

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,20 +1,33 @@
 import webpack from 'webpack';
 import path  from 'path';
 
-export default {
-    entry: path.join(__dirname, 'index.js'),
-    output: {
+const entry = path.join(__dirname, 'index.js');
+const module = {
+    loaders: [{
+        test: /\.js/,
+        loader: 'babel-loader'
+    }]
+};
+const output = (min=false) => {
+    const filename = min?'frontexpress.min.js':'frontexpress.js';
+    return {
         path: __dirname,
-        filename: 'frontexpress.min.js'
-    },
-    module: {
-        loaders: [
-            { test: /\.js/, loader: 'babel-loader' }
-        ]
-    },
+        filename
+    };
+};
+
+export default [{
+    entry,
+    output: output(true),
+    module,
     plugins: [
         new webpack.optimize.DedupePlugin(),
         new webpack.optimize.OccurenceOrderPlugin(),
-        new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+        new webpack.optimize.UglifyJsPlugin(),
     ],
-};
+},
+{
+    entry,
+    output: output(),
+    module
+}];
